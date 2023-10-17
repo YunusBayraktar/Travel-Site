e5cdc1d474982299ca811bbf9890b5fdc9740815
Yunus Bayraktar <bayraktary93@gmail.com>
2023-10-17 19:32:00 +0300

    external js dosyası olusturdum

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+let name = "Ali";
+let surname = "Bayraktar"
+let age= "20";
+let mail = "Bayraktary93@gmail.com";
+ console.log("Name"+":"+ name);
+ console.log("Surname"+":"+surname);
+ console.log("Age"+":"+age);
+ console.log("Mail"+":"+mail);  
+ //    onscroll= "scrollFunction()"
+ //    if(window.pageYOffset>100) {
+ //         // document.getElementById("menu").classList.add("menu-scroll")
+ //      }
+ //      else {
+ //         document.getElementById("menu").classList.remove("menu-scroll")
+ //      }
+ //     //document.getElementById("menu").classList.add("menu-scroll")
+ //document.console.log("scroll,window.pageYOffset")
