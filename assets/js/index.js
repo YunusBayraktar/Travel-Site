c7c64419660b6708b360b808fe5d0bc1d42f3a80
Yunus Bayraktar <bayraktary93@gmail.com>
2023-10-20 13:01:20 +0300

    yorum satırı ekledim

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,3 @@ let mail = "Bayraktary93@gmail.com";
  console.log("Surname"+":"+surname);
  console.log("Age"+":"+age);
  console.log("Mail"+":"+mail);  
- //    onscroll= "scrollFunction()"
- //    if(window.pageYOffset>100) {
- //         // document.getElementById("menu").classList.add("menu-scroll")
- //      }
- //      else {
- //         document.getElementById("menu").classList.remove("menu-scroll")
- //      }
- //     //document.getElementById("menu").classList.add("menu-scroll")
- //document.console.log("scroll,window.pageYOffset")
